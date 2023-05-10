6d80132e28c51bb903576bd1af7dc892f92a66a2
unknown <inriveronika17@gmail.com>
2023-05-10 10:55:14 +0700

    feat tabel

--- src/App.jsx
+++ src/App.jsx
@@ -25,79 +25,108 @@ function App() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <table>
-        <thead>
-          <tr>
-            <th>Mahasiswa</th>
-            <th>Aspek Penilaian 1</th>
-            <th>Aspek Penilaian 2</th>
-            <th>Aspek Penilaian 3</th>
-            <th>Aspek Penilaian 4</th>
-          </tr>
-        </thead>
-        <tbody>
-          {[...Array(10)].map((_, index) => (
-            <tr key={index}>
-              <td>Mahasiswa {index + 1}</td>
-              <td>
-                <input
-                  type="number"
-                  min="1"
-                  max="10"
-                  value={
-                    penilaian.aspek_penilaian_1[`Mahasiswa_${index + 1}`] || ""
-                  }
-                  onChange={(event) =>
-                    handleInputChange(event, "aspek_penilaian_1", index + 1)
-                  }
-                />
-              </td>
-              <td>
-                <input
-                  type="number"
-                  min="1"
-                  max="10"
-                  value={
-                    penilaian.aspek_penilaian_2[`Mahasiswa_${index + 1}`] || ""
-                  }
-                  onChange={(event) =>
-                    handleInputChange(event, "aspek_penilaian_2", index + 1)
-                  }
-                />
-              </td>
-              <td>
-                <input
-                  type="number"
-                  min="1"
-                  max="10"
-                  value={
-                    penilaian.aspek_penilaian_3[`Mahasiswa_${index + 1}`] || ""
-                  }
-                  onChange={(event) =>
-                    handleInputChange(event, "aspek_penilaian_3", index + 1)
-                  }
-                />
-              </td>
-              <td>
-                <input
-                  type="number"
-                  min="1"
-                  max="10"
-                  value={
-                    penilaian.aspek_penilaian_4[`Mahasiswa_${index + 1}`] || ""
-                  }
-                  onChange={(event) =>
-                    handleInputChange(event, "aspek_penilaian_4", index + 1)
-                  }
-                />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <button type="submit">Simpan</button>
-    </form>
+    <div className="w-full h-screen flex justify-center bg-[#e2e8f0]">
+      <div className="items-center mt-36 ">
+        <form onSubmit={handleSubmit}>
+          <table className="border-4 border-indigo-500/75 ">
+            <thead>
+              <tr className="font-bold text-2xl ">
+                <th className="border-4 border-indigo-500/50">Mahasiswa</th>
+                <th className="border-4 border-indigo-500/50 w-40">
+                  Aspek Penilaian 1
+                </th>
+                <th className="border-4 border-indigo-500/50 w-40">
+                  Aspek Penilaian 2
+                </th>
+                <th className="border-4 border-indigo-500/50 w-40">
+                  Aspek Penilaian 3
+                </th>
+                <th className="border-4 border-indigo-500/50 w-40">
+                  Aspek Penilaian 4
+                </th>
+              </tr>
+            </thead>
+            <tbody className="">
+              {[...Array(10)].map((_, index) => (
+                <tr key={index}>
+                  <td className="border-4 border-indigo-500/50">
+                    Mahasiswa {index + 1}
+                  </td>
+                  <td className="border-4 border-indigo-500/50">
+                    <input
+                      type="number"
+                      min="1"
+                      max="10"
+                      className="rounded-lg bg-[#cbd5e1] border-2 focus:outline-none text-black w-full"
+                      value={
+                        penilaian.aspek_penilaian_1[`Mahasiswa_${index + 1}`] ||
+                        ""
+                      }
+                      onChange={(event) =>
+                        handleInputChange(event, "aspek_penilaian_1", index + 1)
+                      }
+                    />
+                  </td>
+                  <td className="border-4 border-indigo-500/50">
+                    <input
+                      type="number"
+                      min="1"
+                      max="10"
+                      className="rounded-lg bg-[#cbd5e1] border-2 focus:outline-none text-black w-full"
+                      value={
+                        penilaian.aspek_penilaian_2[`Mahasiswa_${index + 1}`] ||
+                        ""
+                      }
+                      onChange={(event) =>
+                        handleInputChange(event, "aspek_penilaian_2", index + 1)
+                      }
+                    />
+                  </td>
+                  <td className="border-4 border-indigo-500/50">
+                    <input
+                      type="number"
+                      min="1"
+                      max="10"
+                      className="rounded-lg bg-[#cbd5e1] border-2 focus:outline-none text-black w-full"
+                      value={
+                        penilaian.aspek_penilaian_3[`Mahasiswa_${index + 1}`] ||
+                        ""
+                      }
+                      onChange={(event) =>
+                        handleInputChange(event, "aspek_penilaian_3", index + 1)
+                      }
+                    />
+                  </td>
+                  <td className="border-4 border-indigo-500/50">
+                    <input
+                      type="number"
+                      min="1"
+                      max="10"
+                      className="rounded-lg bg-[#cbd5e1] border-2 focus:outline-none text-black w-full"
+                      value={
+                        penilaian.aspek_penilaian_4[`Mahasiswa_${index + 1}`] ||
+                        ""
+                      }
+                      onChange={(event) =>
+                        handleInputChange(event, "aspek_penilaian_4", index + 1)
+                      }
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="flex justify-end mt-6">
+            <button
+              type="submit"
+              className="bg-black text-white rounded-full w-44"
+            >
+              Simpan
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
